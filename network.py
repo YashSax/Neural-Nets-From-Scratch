5c9d092c3ec969f32642c6c5d550382a6255fe8f
YashSax <yash.saxena@gmail.com>
2024-04-03 10:03:48 -0500

    Added loss

--- network.py
+++ network.py
@@ -8,31 +8,40 @@ class Module(ABC):
     def forward(self, x: np.array):
         pass
 
+class LossFn(ABC):
+    @abstractmethod
+    def __call__(self, y_true: np.array, y_pred: np.array):
+        pass
+
 class Sequential():
-    def __init__(self, modules: List[Module]):
+    def __init__(self, modules: List[Module], criterion: LossFn):
         self.modules = modules
+        self.criterion = criterion
+        self.loss = 0
     
     def forward(self, x: np.array):
         for module in self.modules:
-            print(x)
             x = module.forward(x)
         return x
+    
+    def calculate_loss(self, y_true: np.array, y_pred: np.array):
+        self.loss = self.criterion(y_true, y_pred)
+        return self.loss
 
-class MSELoss():
-    def __call__(self, y_true, y_pred):
+class MSELoss(LossFn):
+    def __call__(self, y_true: np.array, y_pred: np.array):
         return np.sum(np.sqrt(y_true ** 2 + y_pred ** 2))
 
 class Layer(Module):
-    def __init__(self, in_dim: int, out_dim: int, weights=None):
+    def __init__(self, in_dim: int, out_dim: int):
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.grad = np.zeros_like(weights)
-
-        self.weights = weights
-        if self.weights is None:
-            self.weights = np.random.normal(0, 1, (in_dim, out_dim))
+        in_dim_with_bias = self.in_dim + 1
+        self.weights = np.random.normal(0, 1, (in_dim_with_bias, out_dim))
 
     def forward(self, x: np.array):
+        x = np.append(x, [1])
+        print(x.shape, self.weights.shape)
         return x @ self.weights
 
 class ReLU(Module):
@@ -43,16 +52,22 @@ class Softmax(Module):
     def forward(self, x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-model = Sequential([
-    Layer(2, 10),
-    ReLU(),
-    Layer(10, 10),
-    ReLU(),
-    Layer(10, 2),
-    Softmax()
-])
+# Let's write a simple model that predicts its own input
+model = Sequential(
+    [
+        Layer(2, 10),
+        ReLU(),
+        Layer(10, 10),
+        ReLU(),
+        Layer(10, 2)
+    ],
+    criterion=MSELoss()
+)
+
 
 ex_in = np.array([1, 2])
-ex_out = model.forward(ex_in)
+y_pred = model.forward(ex_in)
 
-criterion = MSELoss()
+y_true = ex_in
+loss = model.calculate_loss(y_true, y_pred)
+
