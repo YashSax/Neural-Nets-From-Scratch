0af3934d93b7b4b6ed31bfc128d1748df0baf669
YashSax <yash.saxena@gmail.com>
2024-04-04 00:36:35 -0500

    I think I wrote Sequential backward

--- network.py
+++ network.py
@@ -27,10 +27,29 @@ class Sequential():
     def calculate_loss(self, y_true: np.array, y_pred: np.array):
         self.loss = self.criterion(y_true, y_pred)
         return self.loss
+    
+    def backward(self):
+        reversed_modules = self.modules[::-1]
+
+        loss_grad = self.criterion.backward()
+        reversed_modules[0].backward(loss_grad)
+        
+        for module, next_module in zip(reversed_modules[1:], reversed_modules):
+            module.backward(next_module.grad)
 
 class MSELoss(LossFn):
     def __call__(self, y_true: np.array, y_pred: np.array):
-        return np.sum(np.sqrt(y_true ** 2 + y_pred ** 2))
+        assert len(y_true.shape) == 1, "y_true must be 1-d"
+        assert len(y_pred.shape) == 1, "y_pred must be 1-d"
+        assert y_true.shape == y_pred.shape, "y_true and y_pred must have the same shape!"
+
+        self.y_true = y_true
+        self.y_pred = y_pred
+        return np.sum((y_true - y_pred) ** 2) / len(y_pred)
+
+    def backward(self):
+        return 2 / len(self.y_pred) * (y_true - y_pred)
+    
 
 class Layer(Module):
     def __init__(self, in_dim: int, out_dim: int):
@@ -39,18 +58,25 @@ class Layer(Module):
         in_dim_with_bias = self.in_dim + 1
         self.weights = np.random.normal(0, 1, (in_dim_with_bias, out_dim))
 
+        self.layer_in = None
+        self.layer_out = None
+
     def forward(self, x: np.array):
         x = np.append(x, [1])
-        print(x.shape, self.weights.shape)
-        return x @ self.weights
+        self.layer_in = x
+        self.layer_out = x @ self.weights
+        return self.layer_out
+
 
 class ReLU(Module):
     def forward(self, x: np.array):
         return np.maximum(x, np.zeros_like(x))
 
+
 class Softmax(Module):
     def forward(self, x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
+
 
 # Let's write a simple model that predicts its own input
 model = Sequential(
@@ -70,4 +96,4 @@ y_pred = model.forward(ex_in)
 
 y_true = ex_in
 loss = model.calculate_loss(y_true, y_pred)
-
+model.backward()
