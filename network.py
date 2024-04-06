163b56a491c9ee32d4eeffd7800dc17d989eafb0
YashSax <yash.saxena@gmail.com>
2024-04-05 22:45:11 -0500

    Backwards actually runs

--- network.py
+++ network.py
@@ -31,11 +31,9 @@ class Sequential():
     def backward(self):
         reversed_modules = self.modules[::-1]
 
-        loss_grad = self.criterion.backward()
-        reversed_modules[0].backward(loss_grad)
-        
-        for module, next_module in zip(reversed_modules[1:], reversed_modules):
-            module.backward(next_module.grad)
+        prev_grad = self.criterion.backward()
+        for module in reversed_modules:
+            prev_grad = module.backward(prev_grad)
 
 class MSELoss(LossFn):
     def __call__(self, y_true: np.array, y_pred: np.array):
@@ -48,8 +46,9 @@ class MSELoss(LossFn):
         return np.sum((y_true - y_pred) ** 2) / len(y_pred)
 
     def backward(self):
-        return 2 / len(self.y_pred) * (y_true - y_pred)
-    
+        self.grad = 2 / len(self.y_pred) * (self.y_true - self.y_pred)
+        return self.grad
+
 
 class Layer(Module):
     def __init__(self, in_dim: int, out_dim: int):
@@ -65,17 +64,35 @@ class Layer(Module):
         x = np.append(x, [1])
         self.layer_in = x
         self.layer_out = x @ self.weights
+
         return self.layer_out
+    
+    def backward(self, next_grad: np.array):
+        layer_in_matrix = np.expand_dims(self.layer_in, axis=1)
+        next_grad_matrix = np.expand_dims(next_grad, axis=0)
+        self.grad = layer_in_matrix @ next_grad_matrix
+        out = self.weights @ next_grad
+        out = out[:-1] # Remove bias grad
+        return out
 
 
 class ReLU(Module):
     def forward(self, x: np.array):
+        self.relu_in = x
         return np.maximum(x, np.zeros_like(x))
+    
+    def backward(self, next_grad: np.array):
+        relu_grad = np.where(self.relu_in < 0, 0, 1)
+        self.grad = np.multiply(relu_grad, next_grad)
+        return self.grad
 
 
 class Softmax(Module):
     def forward(self, x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
+    
+    def backward(self, next_grad):
+        raise NotImplementedError()
 
 
 # Let's write a simple model that predicts its own input
@@ -92,7 +109,7 @@ model = Sequential(
 
 
 ex_in = np.array([1, 2])
-y_pred = model.forward(ex_in)
+y_pred = model.forward(np.expand_dims(ex_in, axis=1))
 
 y_true = ex_in
 loss = model.calculate_loss(y_true, y_pred)
